a1055954d879057fc8645d412d67f6ba21456f14
Ajay Pandey4 <ap00901404@techmahindra.com>
2024-08-13 18:34:35 +0530

    updated css for banner and section bg

--- blocks/Banner/Banner.js
+++ blocks/Banner/Banner.js
@@ -1,4 +1,12 @@
 export default function decorate(block) {
-  const cols = [...block.firstElementChild.children];
+  document.title = "EDS Assignment-1"
+  const cols = [...block.firstElementChild.children].forEach((row) => {
+
+  // and give it some content
+  const newContent = document.createTextNode("welcome to eds banner");
+
+  // add the text node to the newly created div
+  row.appendChild(newContent);
+  } );
   
 }
