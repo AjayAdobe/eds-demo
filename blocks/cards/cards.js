0ddc5d85d84f0c10b1407094bfaf7ba4d4dda8e9
Ajay Pandey4 <ap00901404@techmahindra.com>
2024-08-16 19:04:58 +0530

    updated the assignment

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,16 +1,30 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
+import {fetchPlaceholders, getMetadata , createOptimizedPicture } from '../../scripts/aem.js';
 
-export default function decorate(block) {
+export default async function decorate(block) {
   /* change to ul, li */
+  const placeholders = await fetchPlaceholders();
+  const {clickMore} = placeholders;
+
+
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
+    
+    const anchor = document.createElement("a");
+    anchor.href = "https://www.aem.live/developer/placeholders";
+    anchor.target = "_blank"
+    anchor.textContent =  clickMore;
+    anchor.classList.add("anchor-text")
+
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
     ul.append(li);
+    li.append(anchor)
+
+   
   });
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
